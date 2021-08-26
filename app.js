d28021ee58abd04aa477ced5eaf0ceb745adbfa0
satish kumar <vsatishkumar91@users.noreply.github.com>
2021-08-26 12:31:20 +0530

    flipcard logic added

--- app.js
+++ app.js
@@ -1,24 +1,55 @@
-document.addEventListener('DOMContentLoaded', () => {
-
+document.addEventListener("DOMContentLoaded", () => {
   const width = 4;
-  const grid = document.querySelector('.grid')
+  const grid = document.querySelector(".grid");
+  const chosenCards = [];
+  const cards = [];
 
   //create your board
   function createBoard() {
-    const numbers = Array(width*width).fill().map((_, index) => parseInt(index/2) + 1);
-    numbers.sort(()=>Math.random()-0.5);
+    const numbers = Array(width * width)
+      .fill()
+      .map((_, index) => parseInt(index / 2) + 1);
+    numbers.sort(() => Math.random() - 0.5);
 
-    for (let i = 0; i < width*width; i++) {
-      const card = document.createElement('div');
-      card.innerHTML = numbers[i];
-      grid.appendChild(card)
+    for (let i = 0; i < width * width; i++) {
+      const card = document.createElement("div");
+      card.innerHTML = "*";
+      card.setAttribute("data-index", i);
+      card.setAttribute("data-value", numbers[i]);
+      card.addEventListener("click", function (e) {
+        flipCard(card);
+      });
+      cards.push(card);
+      grid.appendChild(card);
     }
   }
 
-  
+  function flipCard(card) {
+    const index = card.getAttribute("data-index");
+    const value = card.getAttribute("data-value");
+    if(chosenCards.length <= 1) {
+      chosenCards.push(index);
+      card.innerHTML = value;
+    }
+
+    if(chosenCards.length== 2) {
+setTimeout(() => {
+  const firstCardIndex = chosenCards[0];
+  const secondCardIndex = chosenCards[1];
+
+
+
+
+  if(ch)
+
+}, 500);
+    }
+    
+  }
+
   function init() {
     createBoard();
   }
 
   init();
-})
+});
