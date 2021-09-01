358a64b00906c9e735fa9215c53aa8659b4b7823
satish kumar <vsatishkumar91@users.noreply.github.com>
2021-09-01 12:43:45 +0530

    flip logic added.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const width = 4;
   const grid = document.querySelector(".grid");
-  const chosenCards = [];
+  const timeCompleted = document.querySelector('#time-completed');
+  let chosenCards = [];
+  let time = 0;
+  let timeInterval;
   const cards = [];
 
   //create your board
@@ -13,9 +16,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     for (let i = 0; i < width * width; i++) {
       const card = document.createElement("div");
-      card.innerHTML = "*";
+      card.innerHTML = numbers[i];
       card.setAttribute("data-index", i);
       card.setAttribute("data-value", numbers[i]);
+      card.classList.add('default');
       card.addEventListener("click", function (e) {
         flipCard(card);
       });
@@ -27,28 +31,63 @@ document.addEventListener("DOMContentLoaded", () => {
   function flipCard(card) {
     const index = card.getAttribute("data-index");
     const value = card.getAttribute("data-value");
-    if(chosenCards.length <= 1) {
+
+    if(card.classList.contains('opened')) return;
+
+    if (chosenCards.length == 1 && chosenCards[0] == index) return;
+
+    if (chosenCards.length <= 1) {
       chosenCards.push(index);
-      card.innerHTML = value;
+      card.classList.add('flip-'+value);
     }
 
-    if(chosenCards.length== 2) {
-setTimeout(() => {
-  const firstCardIndex = chosenCards[0];
-  const secondCardIndex = chosenCards[1];
+    if (chosenCards.length == 2) {
+      setTimeout(() => {
+        const firstCardIndex = chosenCards[0];
+        const secondCardIndex = chosenCards[1];
 
+        const firstCardValue = cards[firstCardIndex].getAttribute("data-value");
+        const secondCardValue = cards[secondCardIndex].getAttribute("data-value");
 
+        if (firstCardValue === secondCardValue) {
+          chosenCards = [];
+          cards[firstCardIndex].innerHTML = firstCardValue;
+          cards[secondCardIndex].innerHTML = secondCardValue;
+          cards[firstCardIndex].classList.add('flip-'+firstCardValue);
+          cards[secondCardIndex].classList.add('flip-'+secondCardValue);
 
+          cards[firstCardIndex].classList.add('opened');
+          cards[secondCardIndex].classList.add('opened');
+        } else {
+          chosenCards = [];
+          cards[firstCardIndex].className = "default";
+          cards[secondCardIndex].className = "default";
+        }
 
-  if(ch)
-
-}, 500);
+      }, 500);
     }
-    
+
   }
+
+  function isGameOver() {
+    isGameOver = true;
+    squares.forEach((item) => {
+      if (item.classList.contains("bomb")) {
+        item.classList.add('blast');
+      }
+    });
+    result.innerHTML = "You loose the game. Better luck next time!";
+    clearInterval(timeInterval);
+  }
+
 
   function init() {
     createBoard();
+    timeCompleted.innerHTML = time;
+    timeInterval = setInterval(() => {
+      time++;
+      timeCompleted.innerHTML = time;
+    }, 1000);
   }
 
   init();
